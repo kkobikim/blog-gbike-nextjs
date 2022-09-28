a075f064bbffa465bacbbcc8ced71dbc63b639e3
kkobikim <108338151+kkobikim@users.noreply.github.com>
2022-09-29 05:52:10 +0900

    d

--- themes/hexo/components/ArticleAdjacent.js
+++ themes/hexo/components/ArticleAdjacent.js
@@ -14,12 +14,22 @@ export default function ArticleAdjacent ({ prev, next }) {
     <Link href={`/article/${prev.slug}`} passHref>
       <a className='py-1  cursor-pointer hover:underline justify-start items-center dark:text-white flex w-full h-full duration-200'>
         <i className='mr-1 fas fa-angle-left' />{prev.title}
-      </a>
+      </a> 
     </Link>
+    <img
+                src={prev?.page_cover}
+                alt={prev.title}
+                className=" max-h-52 h-44 w-full rounded-2lg transform object-cover duration-200"
+              />
     <Link href={`/article/${next.slug}`} passHref>
       <a className='py-1 cursor-pointer hover:underline justify-end items-center dark:text-white flex w-full h-full duration-200'>{next.title}
         <i className='ml-1 my-1 fas fa-angle-right' />
       </a>
     </Link>
+    <img
+                src={next?.page_cover}
+                alt={next.title}
+                className=" max-h-52 h-44 w-full rounded-2lg transform object-cover duration-200"
+              />
   </section>
 }

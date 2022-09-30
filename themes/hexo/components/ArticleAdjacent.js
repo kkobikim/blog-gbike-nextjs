c59f2c05e347448d86ab454647205d6816326615
kkobikim <108338151+kkobikim@users.noreply.github.com>
2022-09-30 16:07:21 +0900

    d

--- themes/hexo/components/ArticleAdjacent.js
+++ themes/hexo/components/ArticleAdjacent.js
@@ -10,27 +10,37 @@ export default function ArticleAdjacent ({ prev, next }) {
   if (!prev || !next || !CONFIG_HEXO.ARTICLE_ADJACENT) {
     return <></>
   }
-  return <section className='text-gray-800 items-center text-xs md:text-sm flex justify-between m-1 '>
+  return <section className=''>
+    <div className='flex md:pr-12 font-medium text-3xl pb-8 pt-10'>추천 포스트</div>
+    <div className='text-gray-800 items-start text-2xl flex flex-col md:flex-row justify-between m-1 pt-5'>
+    <div className='flex flex-col w-full md:flex-1 md:pr-12'>
     <Link href={`/article/${prev.slug}`} passHref>
-      <a className='py-1  cursor-pointer hover:underline justify-start items-center dark:text-white flex w-full h-full duration-200'>
-        <i className='mr-1 fas fa-angle-left' />{prev.title}
-      </a> 
-    </Link>
     <img
                 src={prev?.page_cover}
                 alt={prev.title}
-                className=" max-h-52 h-44 w-full rounded-2lg transform object-cover duration-200"
+                className="cursor-pointer h-44 w-full max-w-full rounded-2lg transform object-cover duration-200"
               />
-    <Link href={`/article/${next.slug}`} passHref>
-      <a className='py-1 cursor-pointer hover:underline justify-end items-center dark:text-white flex w-full h-full duration-200'>{next.title}
-        <i className='ml-1 my-1 fas fa-angle-right' />
-      </a>
+              </Link>
+              <Link href={`/article/${prev.slug}`} passHref>
+      <a className='py-5 cursor-pointer justify-start items-center flex dark:text-white w-full duration-200'>
+        {prev.title}
+      </a> 
     </Link>
+              </div>
+              <div className='flex flex-col w-full flex-none md:flex-1'>
+    <Link href={`/article/${next.slug}`} passHref>
     <img
                 src={next?.page_cover}
                 alt={next.title}
-                className=" max-h-52 h-44 w-full rounded-2lg transform object-cover duration-200"
+                className="cursor-pointer h-44 w-full rounded-2lg transform object-cover duration-200"
               />
-              
+              </Link>
+              <Link href={`/article/${next.slug}`} passHref>
+      <a className='py-5 cursor-pointer justify-start items-center flex dark:text-white w-full duration-200'>{next.title}
+       
+      </a>
+    </Link>
+              </div>
+              </div>
   </section>
 }

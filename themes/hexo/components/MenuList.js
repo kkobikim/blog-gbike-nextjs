e3645ef33ba88e32d70883b502527093d57980e9
kkobikim <108338151+kkobikim@users.noreply.github.com>
2022-10-05 14:32:01 +0900

    d

--- themes/hexo/components/MenuList.js
+++ themes/hexo/components/MenuList.js
@@ -11,10 +11,8 @@ const MenuList = (props) => {
   const archiveSlot = <div className='bg-gray-300 dark:bg-gray-500 rounded-md text-gray-50 px-1 text-xs'>{postCount}</div>
 
   let links = [
-    { icon: 'fas fa-home', name: locale.NAV.INDEX, to: '/' || '/', show: true },
-    { icon: 'fas fa-th', name: locale.COMMON.CATEGORY, to: '/category', show: CONFIG_HEXO.MENU_CATEGORY },
-    { icon: 'fas fa-archive', name: locale.NAV.ARCHIVE, to: '/archive', slot: archiveSlot, show: CONFIG_HEXO.MENU_ARCHIVE },
-    { icon: 'fas fa-search', name: locale.NAV.SEARCH, to: '/search', show: CONFIG_HEXO.MENU_SEARCH }
+    { icon: 'fas fa-search', name: locale.NAV.SEARCH, to: '/search', show: CONFIG_HEXO.MENU_SEARCH },
+    { icon: 'fas fa-archive', name: locale.NAV.ARCHIVE, to: '/archive', slot: archiveSlot, show: CONFIG_HEXO.MENU_ARCHIVE }
   ]
   if (customNav) {
     links = links.concat(customNav)
@@ -34,8 +32,6 @@ const MenuList = (props) => {
             {link.slot}
           </a>
         </Link>
-      } else {
-        return null
       }
     })}
   </nav>

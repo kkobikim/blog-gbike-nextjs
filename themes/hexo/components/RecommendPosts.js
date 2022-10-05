a30ea57df6b46ea5a3d7c634c828ed040caccdd8
kkobikim <108338151+kkobikim@users.noreply.github.com>
2022-10-05 12:23:22 +0900

    a

--- themes/hexo/components/RecommendPosts.js
+++ themes/hexo/components/RecommendPosts.js
@@ -12,15 +12,35 @@ const RecommendPosts = ({ recommendPosts }) => {
   const { locale } = useGlobal()
 
   return (
-    <div className="pt-2 border pl-4 py-2 my-4 dark:text-gray-300 ">
-       <div className="mb-2 font-bold text-lg">{locale.COMMON.RELATE_POSTS} :</div>
+    <div className="pt-2 pl-4 py-2 my-4 dark:text-gray-300 ">
+       <div className='md:pr-12 font-medium dark:text-white text-3xl pb-8 pt-20'>새로운 포스트</div>
         <ul className="font-light text-sm">
           {recommendPosts.map(post => (
-            <li className="py-1" key={post.id}>
+            <li className="py-1 flex flex-row-reverse justify-between pb-14" key={post.id}>
+                <img
+                src={post?.page_cover}
+                alt={post.title}
+                className="cursor-pointer h-20 md:h-40 w-1/3 rounded-2lg transform object-cover duration-200 border-white md:hover:-translate-y-2 md:hover:shadow-xl"
+                
+              />
               <Link href={`/article/${post.slug}`}>
-                <a className="cursor-pointer hover:underline">
-                  {post.title}
-                </a>
+              <div className="flex flex-col w-full pr-8">
+              <a className="cursor-pointer my-2 font-light text-gray-500 dark:text-gray-300 text-sm transform">
+                {post.category}
+              </a>
+            <a
+              className={`replace cursor-pointer notion-card-text text-xl font-sans leading-tight font-medium text-gray-700 dark:text-gray-100`}
+            >
+              {post.title}
+            </a>
+            <div
+            className={`flex mt-2 items-center flex-wrap dark:text-gray-500 text-gray-400`}
+             >
+              <a className="font-light cursor-pointer text-sm leading-4 mr-3">
+                {post.date?.start_date || post.lastEditedTime}
+              </a>
+          </div>
+            </div>
               </Link>
             </li>
           ))}
--- themes/hexo/components/ShareBar.js
+++ themes/hexo/components/ShareBar.js
@@ -1,9 +1,8 @@
 import BLOG from '@/blog.config'
 import { useRouter } from 'next/router'
 import React from 'react'
-import { createPopper } from '@popperjs/core'
+import Link from 'next/link'
 import copy from 'copy-to-clipboard'
-import QRCode from 'qrcode.react'
 import { useGlobal } from '@/lib/global'
 
 
@@ -23,13 +22,22 @@ const ShareBar = ({ post }) => {
 
   return <>
     <div
-      className='py-2 text-gray-500 text-center space-x-2 flex my-1 dark:text-gray-200'>
+      className='py-2 w-full flex justify-between text-gray-700 text-center space-x-2 flex my-1 dark:text-gray-200'>
    
-      <div className='cursor-pointer text-2xl'>
-        <a className='text-yellow-600' onClick={copyUrl} >
-          <i className='fas fa-link'/>
+      <div className='cursor-pointer text-sm bg-gray-200 border py-2 px-3 rounded-md'>
+        <a className='' onClick={copyUrl} >
+          <i className='fas fa-link pr-1'/> 공유하기
         </a>
       </div>
+
+      <div className='cursor-pointer text-sm bg-gray-200 border py-2 px-3 rounded-md'>
+      <Link href='mailto:kkobi.kim@gbike.io' passHref>
+        <a target="_blank">
+          <i className='fas fa-envelope pr-1'/> 의견 보내기
+        </a>
+        </Link>
+      </div>
+
     </div>
   </>
 }
